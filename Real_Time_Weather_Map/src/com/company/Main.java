2a998759010988be83991980db8c5b908adb26b3
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2020-09-01 11:15:44 -0400

    Added capitals and state names to HashMap

--- Real_Time_Weather_Map/src/com/company/Main.java
+++ Real_Time_Weather_Map/src/com/company/Main.java
@@ -1,8 +1,60 @@
 package com.company;
 
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-	// Open Weather api key: 298871d88f2037555322fa3baed94e56
+        HashMap<String, String> capitals = new HashMap<>();
+        capitals.put("Montgomery","Alabama");
+        capitals.put("Junea","Alaska");
+        capitals.put("Phoenix","Arizona");
+        capitals.put("Little Rock","Arkansas");
+        capitals.put("Sacramento","California");
+        capitals.put("Denver","Colorado");
+        capitals.put("Hartford","Connecticut");
+        capitals.put("Dover","Delaware");
+        capitals.put("Tallahassee","Florida");
+        capitals.put("Atlanta","Georgia");
+        capitals.put("Honolulu","Hawaii");
+        capitals.put("Boise","Idaho");
+        capitals.put("Springfield","Illinois");
+        capitals.put("Indianapolis","Indiana");
+        capitals.put("Des Moines","Iowa");
+        capitals.put("Topeka","Kansas");
+        capitals.put("Frankfort","Kentucky");
+        capitals.put("Baton Rouge","Louisiana");
+        capitals.put("Augusta","Maine");
+        capitals.put("Annapolis","Maryland");
+        capitals.put("Boston","Massachusetts");
+        capitals.put("Lansing","Michigan");
+        capitals.put("Saint Paul","Minnesota");
+        capitals.put("Jackson","Mississippi");
+        capitals.put("Jefferson City","Missouri");
+        capitals.put("Helena","Montana");
+        capitals.put("Lincoln","Nebraska");
+        capitals.put("Carson City","Nevada");
+        capitals.put("Concord","New Hampshire");
+        capitals.put("Trenton","New Jersey");
+        capitals.put("Santa Fe","New Mexico");
+        capitals.put("Albany","New York");
+        capitals.put("Raleigh","North Carolina");
+        capitals.put("Bismarck","North Dakota");
+        capitals.put("Columbus","Ohio");
+        capitals.put("Oklahoma City","Oklahoma");
+        capitals.put("Salem","Oregon");
+        capitals.put("Harrisburg","Pennsylvania");
+        capitals.put("Providence","Rhode Island");
+        capitals.put("Columbia","South Carolina");
+        capitals.put("Pierre","South Dakota");
+        capitals.put("Nashville","Tennessee");
+        capitals.put("Austin","Texas");
+        capitals.put("Salt Lake City","Utah");
+        capitals.put("Montpelier","Vermont");
+        capitals.put("Richmond","Virgina");
+        capitals.put("Olympia","Washington");
+        capitals.put("Charleston","West Virginia");
+        capitals.put("Madison","Wisconsin");
+        capitals.put("Cheyenne","Wyoming");
     }
 }
